59e37f66bd72f6d75461fa5b63e88df2885f8c84
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-08-05 22:28:38 +0000

    2.0.0-rc.1

--- app/src/main/java/com/android/apksig/internal/pkcs7/IssuerAndSerialNumber.java
+++ app/src/main/java/com/android/apksig/internal/pkcs7/IssuerAndSerialNumber.java
@@ -11,3 +11,33 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.apksig.internal.pkcs7;
+
+import com.android.apksig.internal.asn1.Asn1Class;
+import com.android.apksig.internal.asn1.Asn1Field;
+import com.android.apksig.internal.asn1.Asn1OpaqueObject;
+import com.android.apksig.internal.asn1.Asn1Type;
+import java.math.BigInteger;
+
+/**
+ * PKCS #7 {@code IssuerAndSerialNumber} as specified in RFC 5652.
+ */
+@Asn1Class(type = Asn1Type.SEQUENCE)
+public class IssuerAndSerialNumber {
+
+    @Asn1Field(index = 0, type = Asn1Type.ANY)
+    public Asn1OpaqueObject issuer;
+
+    @Asn1Field(index = 1, type = Asn1Type.INTEGER)
+    public BigInteger certificateSerialNumber;
+
+    public IssuerAndSerialNumber() {}
+
+    public IssuerAndSerialNumber(Asn1OpaqueObject issuer, BigInteger certificateSerialNumber) {
+        this.issuer = issuer;
+        this.certificateSerialNumber = certificateSerialNumber;
+    }
+}
