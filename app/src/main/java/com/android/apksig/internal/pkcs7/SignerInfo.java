0458f6d431abb7e643b9393f993157f6d16836c0
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-08-07 10:24:06 +0000

    NR-408 : Instagram support in LD sources

--- app/src/main/java/com/android/apksig/internal/pkcs7/SignerInfo.java
+++ app/src/main/java/com/android/apksig/internal/pkcs7/SignerInfo.java
@@ -28,4 +28,34 @@ import java.util.List;
  * PKCS #7 {@code SignerInfo} as specified in RFC 5652.
  */
 @Asn1Class(type = Asn1Type.SEQUENCE)
-public c
+public class SignerInfo {
+
+    @Asn1Field(index = 0, type = Asn1Type.INTEGER)
+    public int version;
+
+    @Asn1Field(index = 1, type = Asn1Type.CHOICE)
+    public SignerIdentifier sid;
+
+    @Asn1Field(index = 2, type = Asn1Type.SEQUENCE)
+    public AlgorithmIdentifier digestAlgorithm;
+
+    @Asn1Field(
+            index = 3,
+            type = Asn1Type.SET_OF,
+            tagging = Asn1Tagging.IMPLICIT, tagNumber = 0,
+            optional = true)
+    public Asn1OpaqueObject signedAttrs;
+
+    @Asn1Field(index = 4, type = Asn1Type.SEQUENCE)
+    public AlgorithmIdentifier signatureAlgorithm;
+
+    @Asn1Field(index = 5, type = Asn1Type.OCTET_STRING)
+    public ByteBuffer signature;
+
+    @Asn1Field(
+            index = 6,
+            type = Asn1Type.SET_OF,
+            tagging = Asn1Tagging.IMPLICIT, tagNumber = 1,
+            optional = true)
+    public List<Attribute> unsignedAttrs;
+}
