ff31a090096d335ae0ba421e9ea80ce5a59dff90
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-08-20 07:38:42 +0000

    Fix sql

--- app/src/main/java/com/android/apksig/internal/util/ByteStreams.java
+++ app/src/main/java/com/android/apksig/internal/util/ByteStreams.java
@@ -16,18 +16,12 @@
 
 package com.android.apksig.internal.util;
 
-import java.nio.ByteBuffer;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-public final class ByteBufferUtils {
-    private ByteBufferUtils() {}
-
-    /**
-     * Returns the remaining data of the provided buffer as a new byte array and advances the
-     * position of the buffer to the buffer's limit.
-     */
-    public static byte[] toByteArray(ByteBuffer buf) {
-        byte[] result = new byte[buf.remaining()];
-        buf.get(result);
-        return result;
-    }
-}
+/**
+ * Utilities for byte arrays and I/O streams.
+ */
+public final class ByteStreams {
+    private ByteStream
