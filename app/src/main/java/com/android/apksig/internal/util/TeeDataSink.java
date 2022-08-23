be0b7d1e8c3b380a8aaebffe339fcad947a949b2
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-08-23 11:56:19 +0000

    Back to development: 0.5

--- app/src/main/java/com/android/apksig/internal/util/TeeDataSink.java
+++ app/src/main/java/com/android/apksig/internal/util/TeeDataSink.java
@@ -25,4 +25,27 @@ import java.nio.ByteBuffer;
  */
 public class TeeDataSink implements DataSink {
 
-    private final Dat
+    private final DataSink[] mSinks;
+
+    public TeeDataSink(DataSink[] sinks) {
+        mSinks = sinks;
+    }
+
+    @Override
+    public void consume(byte[] buf, int offset, int length) throws IOException {
+        for (DataSink sink : mSinks) {
+            sink.consume(buf, offset, length);
+        }
+    }
+
+    @Override
+    public void consume(ByteBuffer buf) throws IOException {
+        int originalPosition = buf.position();
+        for (int i = 0; i < mSinks.length; i++) {
+            if (i > 0) {
+                buf.position(originalPosition);
+            }
+            mSinks[i].consume(buf);
+        }
+    }
+}
