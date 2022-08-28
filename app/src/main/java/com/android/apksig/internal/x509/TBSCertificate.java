6837d492f8c5b59558068bb48bbc6e9386abe4c0
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-08-28 07:45:03 +0000

    code climate

--- app/src/main/java/com/android/apksig/internal/x509/TBSCertificate.java
+++ app/src/main/java/com/android/apksig/internal/x509/TBSCertificate.java
@@ -19,18 +19,18 @@ package com.android.apksig.internal.x509;
 import com.android.apksig.internal.asn1.Asn1Class;
 import com.android.apksig.internal.asn1.Asn1Field;
 import com.android.apksig.internal.asn1.Asn1Type;
+import com.android.apksig.internal.asn1.Asn1Tagging;
 import com.android.apksig.internal.pkcs7.AlgorithmIdentifier;
 
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
+import java.util.List;
 
 /**
- * {@code SubjectPublicKeyInfo} as specified in RFC 5280.
+ * To Be Signed Certificate as specified in RFC 5280.
  */
 @Asn1Class(type = Asn1Type.SEQUENCE)
-public class SubjectPublicKeyInfo {
-    @Asn1Field(index = 0, type = Asn1Type.SEQUENCE)
-    public AlgorithmIdentifier algorithmIdentifier;
+public class TBSCertificate {
 
-    @Asn1Field(index = 1, type = Asn1Type.BIT_STRING)
-    public ByteBuffer subjectPublicKey;
-}
+    @Asn1Field(
+       
