be47ace98f9dbfd8608cc035848033ff7e99ae57
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-10-31 18:02:27 +0000

    Ignore built gems

--- app/src/main/java/com/zane/smapiinstaller/ui/download/DownloadableContentAdapter.java
+++ app/src/main/java/com/zane/smapiinstaller/ui/download/DownloadableContentAdapter.java
@@ -18,4 +18,62 @@ import com.zane.smapiinstaller.databinding.DownloadContentItemBinding;
 import com.zane.smapiinstaller.entity.DownloadableContent;
 import com.zane.smapiinstaller.entity.ModManifestEntry;
 import com.zane.smapiinstaller.logic.ModAssetsManager;
-import com.zane.smapiinstaller.utils.DialogUtil
+import com.zane.smapiinstaller.utils.DialogUtils;
+import com.zane.smapiinstaller.utils.FileUtils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.zeroturnaround.zip.ZipUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DownloadableContent}
+ *
+ * @author Zane
+ */
+public class DownloadableContentAdapter extends RecyclerView.Adapter<DownloadableContentAdapter.ViewHolder> {
+
+    private List<DownloadableContent> downloadableContentList;
+
+    public void setDownloadableContentList(List<DownloadableContent> downloadableContentList) {
+        this.downloadableContentList = downloadableContentList;
+        notifyDataSetChanged();
+    }
+
+    public DownloadableContentAdapter(List<DownloadableContent> items) {
+        downloadableContentList = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.download_content_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.setDownloadableContent(downloadableContentList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return downloadableContentList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final DownloadContentItemBinding binding;
+
+        private final AtomicBoolean downloading = new AtomicBoolean(false);
+
+        public DownloadableContent downloadableContent;
+
+        public void setDownloadableContent(Downloadab
