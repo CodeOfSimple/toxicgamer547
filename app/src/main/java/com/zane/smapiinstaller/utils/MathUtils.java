dbf5b13d3d9e7df885f782ed094bdff976d338ef
CodeOfSimple <119113810+CodeOfSimple@users.noreply.github.com>
2022-12-10 16:45:53 +0000

    \tmodified:   setup.py

---
+++ app/src/main/java/com/zane/smapiinstaller/utils/MathUtils.java
@@ -0,0 +1,12 @@
+package com.zane.smapiinstaller.utils;
+
+import java.math.BigInteger;
+
+/**
+ * @author Zane
+ */
+public class MathUtils {
+
+    private static final int INT_MAX_BIT_VALUE = 31;
+
+    private static
